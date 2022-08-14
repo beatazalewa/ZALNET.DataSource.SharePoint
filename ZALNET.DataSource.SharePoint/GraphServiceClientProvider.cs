4cceebbbb1f8effee7d23452651e33089c2ea259
Beata Zalewa <beata.zalewa@gmail.com>
2022-08-14 13:15:13 +0200

    Added new class

--- ZALNET.DataSource.SharePoint/GraphServiceClientProvider.cs
+++ ZALNET.DataSource.SharePoint/GraphServiceClientProvider.cs
@@ -2,8 +2,6 @@
 using Microsoft.Graph;
 using Microsoft.Graph.Auth;
 using Microsoft.Identity.Client;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
 using ZALNET.DataSource.SharePoint.Settings;
 
 namespace ZALNET.DataSource.SharePoint
@@ -27,30 +25,5 @@ namespace ZALNET.DataSource.SharePoint
             var authProvider = new ClientCredentialProvider(confidentialClientApplication);
             return new GraphServiceClient(authProvider);
         }
-        //            // The Azure AD tenant ID  (e.g. tenantId.onmicrosoft.com)
-        //            var tenantId = _azureAppSettings.TenantId;
-        //            // The client ID of the app registered in Azure AD
-        //            var clientId = _azureAppSettings.ClientId;
-        //            // Application Client Secret (Recommended this is stored safely and not hardcoded)
-        //            var clientSecret = _azureAppSettings.ClientSecret;
-        //            var scopes = new string[] { "https://graph.microsoft.com/.default" };
-        //            var confidentialClient = ConfidentialClientApplicationBuilder
-        //                .Create(clientId)
-        //                .WithAuthority($"https://login.microsoftonline.com/$tenantId/v2.0")
-        //                .WithClientSecret(clientSecret)
-        //                .Build();
-        //            GraphServiceClient graphServiceClient =
-        //new GraphServiceClient(new DelegateAuthenticationProvider(async (requestMessage) => {
-
-        //    // Retrieve an access token for Microsoft Graph (gets a fresh token if needed).
-        //    var authResult = await confidentialClient.AcquireTokenForClient(scopes).ExecuteAsync();
-
-        //    // Add the access token in the Authorization header of the API
-        //    requestMessage.Headers.Authorization =
-        //    new AuthenticationHeaderValue("Bearer", authResult.AccessToken);
-        //}));
-        //            // Make a Microsoft Graph API query
-        //            var users = await graphServiceClient.Users.Request().GetAsync();
-        //            return (GraphServiceClient)users;
     }
 }
--- ZALNET.DataSource.SharePoint/MsGraphConfiguration.cs
+++ ZALNET.DataSource.SharePoint/MsGraphConfiguration.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ZALNET.DataSource.SharePoint
 {
-    internal class MsGraphConfiguration
-    {
+    internal class MsGraphConfiguration 
+    { 
+        [Required] 
+        public string TenantId { get; set; } 
+        [Required]
+        public string CredentialsClientId { get; set; }
+        [Required]
+        public string CredentialsClientSecret { get; set; }
+        [Required] 
+        public string TenantName { get; set; } 
     }
 }
